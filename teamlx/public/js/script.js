aecfb015fa6533e8fd158ef729d1f69867ff78cd
Aleksandra Wos <aleksandra1.wos@gmail.com>
2022-02-12 16:29:54 +0000

    Framework

---
+++ teamlx/public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("teamlx JS imported successfully!");
+  },
+  false
+);
---
+++ teamlx/utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/teamlx";
+
+module.exports = MONGO_URI;
