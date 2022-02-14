49521fcdad728e99ae219467112eadccbdea5efd
pavaoana <anapavao2@gmail.com>
2022-02-14 08:29:17 +0000

    demo commit 4

--- teamlx/routes/auth.js
+++ teamlx/routes/auth.js
@@ -14,11 +14,11 @@ const User = require("../models/User.model");
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
-let a = 4 
-console.log(a)
+let a = 4;
+console.log(a);
 router.get("/signup", isLoggedOut, (req, res) => {
-  let a = 4;
-  console.log(a);
+  const a = 2;
+  const b = a * 2;
 
   res.render("auth/signup");
 });
