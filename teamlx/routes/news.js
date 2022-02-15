d3f43ff6033d9e96e6fa5fbb39403438294de8c2
pavaoana <anapavao2@gmail.com>
2022-02-15 10:53:54 +0000

    in order to be able to pull

--- teamlx/routes/news.js
+++ teamlx/routes/news.js
@@ -1,25 +1,29 @@
 const router = require("express").Router();
-const NewsAPI = require('newsapi');
+const NewsAPI = require("newsapi");
 
-router.get("/", (req, res) =>{
+router.get("/", (req, res) => {
+  // res.send("news");
+  res.render("post/news-thread");
 
-    res.send("news")
+  const newsapi = new NewsAPI(process.env.API_KEY);
 
-    const newsapi = new NewsAPI(process.env.API_KEY);
-
-    newsapi.v2.everything({
-        q: 'bitcoin',
-        language: 'pl',    
+  newsapi.v2
+    .everything({
+      q: "bitcoin",
+      language: "en",
     })
-    .then(data=>{
-       res.render("post/news-thread")
-    })
+    .then((data) => {
+      console.log(data);
+      res.render("post/news-thread");
+    });
+});
 
-
-})
-
-
-
-
+// const search = document.querySelector(".search");
+// const input = document.querySelector(".input");
+// const listOfNews = document.querySelector(".news-list");
+// search.addEventListener("submit", retrieve)
+// function retrieve(event) {
+//     const apiKey =
+// }
 
 module.exports = router;
